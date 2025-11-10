e8991a01a7c834831aa9f5bbe8f7986238622fa2
Siri Manoj <sirimanoj1465@gmail.com>
2025-11-10 21:25:11 +0000

    The Cloud Build with ID 4403effb-85da-4cfa-a060-f99c45c46579 failed due

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,17 +3,19 @@ import {NextIntlClientProvider} from 'next-intl';
 import {getMessages} from 'next-intl/server';
 import AppLayout from '../(app)/layout';
  
-export default async function LocaleLayout({
-  children,
-  params: {locale}
-}: {
+type LocaleLayoutProps = {
   children: React.ReactNode;
   params: {locale: string};
-}) {
+}
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: LocaleLayoutProps) {
   const messages = await getMessages();
 
   return (
-    <NextIntlClientProvider locale={locale} messages={messages}>
+    <NextIntlClientProvider locale={params.locale} messages={messages}>
       <AppLayout>
         {children}
       </AppLayout>

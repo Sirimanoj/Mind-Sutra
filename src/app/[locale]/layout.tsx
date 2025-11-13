df613e231f7084e6abca3e7e9e6358faa0c94603
Siri Manoj <sirimanoj1465@gmail.com>
2025-11-13 20:20:00 +0000

    you see we are using next 15 so i think params should be a promise no

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { type ReactNode } from 'react';
 import {NextIntlClientProvider} from 'next-intl';
 import {getMessages} from 'next-intl/server';
 import AppLayout from '../(app)/layout';
 
 type LocaleLayoutProps = {
-  children: React.ReactNode;
+  children: ReactNode;
   params: { locale: string; };
 }
 
 export default async function LocaleLayout({
   children,
-  params: { locale },
+  params,
 }: LocaleLayoutProps) {
   const messages = await getMessages();
+  const { locale } = params;
  
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>

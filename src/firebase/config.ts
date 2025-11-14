4d7d62d035814e2ec6b46afd28c52f0dc59fef7c
Siri Manoj <sirimanoj1465@gmail.com>
2025-11-14 07:09:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9057087504-c0bb3",
+  "appId": "1:564574159159:web:d9eb6097ce9f57babc5ab2",
+  "apiKey": "AIzaSyDlqPdsfd_Unq4zk-pf_0AJCDdKxDCNuao",
+  "authDomain": "studio-9057087504-c0bb3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "564574159159"
+};
